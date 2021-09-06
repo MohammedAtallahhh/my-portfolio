02bf19f4a883a64c88102c7c5468f8924f3ecc56
Mohammad Atallah <mohamedatallahh2017@gmail.com>
2021-09-06 16:39:04 +0300

    Added Hero and change the font family && Made the nav responsive

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Hero.css";
+
+import { FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+
+export const Hero = () => {
+  return (
+    <main className="hero">
+      <div className="hero--email">mohamedatallahh2017@gmail.com</div>
+      <div className="hero--contact-links">
+        <a href="#a" className="twitter">
+          <FaTwitter />
+        </a>
+        <a href="#a" className="linkedin">
+          <FaLinkedinIn />
+        </a>
+        <a href="#a" className="github">
+          <FiGithub />
+        </a>
+      </div>
+      <div className="container">
+        <div className="hero--text">
+          <h2 className="hero--text__hello">Hello, I am</h2>
+          <h1 className="hero--text__mohammed">Mohammed</h1>
+          <h1 className="hero--text__Atallah">Atallah</h1>
+          <h2 className="hero--text__title">Front-end Web developer.</h2>
+
+          <button className="hero--text__cta">let's talk</button>
+        </div>
+      </div>
+    </main>
+  );
+};
